ffa0fcdb5ae42df5f79420dc19fc30e455e214bb
Luciano Wayand de Abreu <36968613+lucianowayand@users.noreply.github.com>
2022-09-26 22:37:33 -0300

    Update main.go

--- main.go
+++ main.go
@@ -1,17 +1,9 @@
 package main
 
 import (
-	"ann/nonlinearsystems"
+	
 )
 
-func f(x float64, y float64) float64  { return x*x + y*y - 5 }
-func fx(x float64, y float64) float64 { return 2 * x }
-func fy(x float64, y float64) float64 { return 2 * y }
-
-func g(x float64, y float64) float64  { return x*x + x*y*y*y - 3 }
-func gx(x float64, y float64) float64 { return 2 * x + y*y*y }
-func gy(x float64, y float64) float64 { return 3 * x*y*y }
-
 func main() {
-	nonlinearsystems.Newton(f, fx, fy, g, gx, gy, 5, -2.1985,0.598)
+	
 }
