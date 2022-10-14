83ed80f25f96381cc9cd9d75bc64f72758ac5bcf
Luciano <lucianowayand@hotmail.com>
2022-10-14 10:50:56 +0000

    refactor: updated some functions on linearsystems module

--- linearsystems/linearsystems.go
+++ linearsystems/linearsystems.go
@@ -2,6 +2,8 @@ package linearsystems
 
 import (
 	"fmt"
+
+	"gonum.org/v1/gonum/mat"
 )
 
 const SIZE = 12
@@ -12,54 +14,55 @@ type Matrix struct {
 	Matrix [SIZE1][SIZE2]float64
 }
 
-func FormatMatrixResult(matrix [SIZE1][SIZE2]float64) {
-	for position := range matrix {
-		for _, element := range matrix[position] {
-			fmt.Printf("%.8f, ", element)
-		}
-	}
-
+func FormatMatrixResult(matrix mat.Dense) {
+	fa := mat.Formatted(&matrix, mat.Prefix(""), mat.Squeeze())
+	fmt.Printf("%v\n", fa)
 }
 
-func CreateMatrix(array [SIZE1 * SIZE2]float64) [SIZE1][SIZE2]float64 {
-	count := 0
-	var matrix [SIZE1][SIZE2]float64
+func CreateMatrix(array []float64, col int, row int) *mat.Dense {
+	matrix := mat.NewDense(row, col, array)
+	return matrix
+}
 
-	for i := 0; i < SIZE1; i++ {
-		for j := 0; j < SIZE2; j++ {
-			matrix[i][j] = array[count]
-			count++
-		}
+func getVectorFromRow(row mat.Vector) []float64{
+	v := make([]float64, row.Len())
+	for i := 0; i < row.Len(); i++ {
+		v[i] = row.At(i,0)
 	}
+	return v
+}
+
+func ChangeLines(matrix mat.Dense, line1 int, line2 int) mat.Dense {
+	firstLine := getVectorFromRow(matrix.RowView(line1-1))
+	secondLine := getVectorFromRow(matrix.RowView(line2-1))
+
+	matrix.SetRow(line2-1, firstLine)
+	matrix.SetRow(line1-1, secondLine)
 
 	return matrix
 }
 
-func ChangeLines(matrix [SIZE1][SIZE2]float64, line1 int, line2 int) [SIZE1][SIZE2]float64 {
-	aux := matrix
+func MultiplyLineElements(matrix mat.Dense, line int, constant float64) mat.Dense {
+	new_row := getVectorFromRow(matrix.RowView(line-1))
+	
+	for index, elements := range new_row{
+		new_row[index] = elements * constant
+	}
 
-	matrix[line1-1] = aux[line2-1]
-	matrix[line2-1] = aux[line1-1]
+	matrix.SetRow(line-1, new_row)
 
 	return matrix
 }
 
-func MultiplyLineElements(matrix [SIZE1][SIZE2]float64, line int, constant float64) [SIZE1][SIZE2]float64 {
-	for position, element := range matrix[line-1] {
-		matrix[line-1][position] = element * constant
-	}
+func AddLineElements(matrix mat.Dense, line1 int, line2 int) mat.Dense {
+	firstLine := mat.VecDenseCopyOf(matrix.RowView(line1-1))
+	secondLine := mat.VecDenseCopyOf(matrix.RowView(line2-1))
+
+	newRow := 
+
+	matrix.SetRow(line-1, firstLine)
+
 	return matrix
-
-}
-
-func AddLineElements(matrix [SIZE1][SIZE2]float64, line1 int, line2 int) [SIZE1][SIZE2]float64 {
-	aux := matrix
-
-	for position := range matrix {
-		aux[line1-1][position] = matrix[line1-1][position] + matrix[line2-1][position]
-	}
-
-	return aux
 }
 
 func AddLineWeightedElements(matrix [SIZE1][SIZE2]float64, line1 int, line2 int, weight float64) [SIZE1][SIZE2]float64 {
@@ -98,31 +101,31 @@ func Jacobi(matrix [SIZE1][SIZE2]float64, results [SIZE1]float64, estimation [SI
     }
 }
 
-func Gauss(matrix [SIZE1][SIZE2]float64){
-	for j:=0; j<SIZE2; j++{
-		for i:=j;j<SIZE2; i++{
-			if matrix[i][j] != 0{
-				if i!=j {
-					var temp float64
-					for k:=0; k<SIZE1; k++{
-						temp = matrix[i][k]
-						matrix[i][k] = matrix[j][k]
-						matrix[j][k] = temp
-					}
-				}
-				for m:=j+1; m<SIZE1; m++{
-					a := float64(-matrix[m][j])/float64(matrix[j][j])
-					for n:=j; n<SIZE1; n++{
-						matrix[m][n] += a * matrix[j][n]
-					}
-					FormatMatrixResult(matrix)
-					break
-				}
-			} else {
-				if (i==SIZE2-1){
-                    fmt.Println("Sistema não possui solução")
-                }
-			}
-		}
-	}
-}
+// func Gauss(matrix [SIZE1][SIZE2]float64){
+// 	for j:=0; j<SIZE2; j++{
+// 		for i:=j;j<SIZE2; i++{
+// 			if matrix[i][j] != 0{
+// 				if i!=j {
+// 					var temp float64
+// 					for k:=0; k<SIZE1; k++{
+// 						temp = matrix[i][k]
+// 						matrix[i][k] = matrix[j][k]
+// 						matrix[j][k] = temp
+// 					}
+// 				}
+// 				for m:=j+1; m<SIZE1; m++{
+// 					a := float64(-matrix[m][j])/float64(matrix[j][j])
+// 					for n:=j; n<SIZE1; n++{
+// 						matrix[m][n] += a * matrix[j][n]
+// 					}
+// 					FormatMatrixResult(matrix)
+// 					break
+// 				}
+// 			} else {
+// 				if (i==SIZE2-1){
+//                     fmt.Println("Sistema não possui solução")
+//                 }
+// 			}
+// 		}
+// 	}
+// }
--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"ann/linearsystems"
 
-func main(){
-	fmt.Println("Main!")
+)
+
+func main() {
+	input := []float64{
+		7, -2, 2, -5,
+		8, 6, -8, 6, 
+		5, 8, 6, 6, 
+		-2, -3, 4, 4,
+	}
+
+	matrix := linearsystems.CreateMatrix(input, 4, 4)
+	linearsystems.FormatMatrixResult(*matrix)
+
+	op1 := linearsystems.ChangeLines(*matrix, 1, 2)
+	linearsystems.FormatMatrixResult(op1)
+
+	op2 := linearsystems.MultiplyLineElements(op1, 3, 10)
+	linearsystems.FormatMatrixResult(op2)
 }
